6cf1d8ecdd3c5d55063440a02ef64238352c96e4
mwebazeallangideon-maker <mwebazeallangideon@gmail.com>
2026-03-11 23:33:23 +0300

    Initialize Flask app and import modules

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+import home_page
+import about_page
+import events_page
+import news_page
+import programmes
+import photo_gallery
+import volunteer_registration
+import admin_dashboard
+import add_news
+import add_events
